c0cc195a795a3a70bb6f3bfb114b0cdbf37725da
Neil Bezuidenhout <34944157+TheOtherNeo@users.noreply.github.com>
2023-12-01 16:30:22 +0400

    Placing back changes

--- draw_canvas.js
+++ draw_canvas.js
@@ -7,64 +7,95 @@ Crated by Neil Bezuidenhout
 20 November 2023
 */
 
-function setup_canvas() {
-    // Set up the canvas for drawings
-    var canvas = document.getElementById("negative_phase_sequence");
+// Global variables will be defined with all caps
+let VECTORS = [];
 
-    window.translatePos = {
-        x: canvas.width / 2,
-        y: canvas.height / 2
-    };
+class Point {
+    constructor(x, y) {
+      this.x = x;
+      this.y = y;
+    }
+  
+    static displayName = "Point";
+    static distance(a, b) {
+      const dx = a.x - b.x;
+      const dy = a.y - b.y;
+  
+      return Math.hypot(dx, dy);
+    }
+  }
 
-    window.scale = 1.0;
-    var scaleMultiplier = 0.8;
-    var startDragOffset = {};
-    var mouseDown = false;
-}
+class Line {
 
-// Draw a line on the canvas in x,y start to x,y end coordinates
-function draw_line({context, start, end, color = 'black', dash = "solid", width = 1, cap = "round", invert_y = true, }) {
+    // Invert all y values.
+    static invert_y = true;
 
     // These are the allowed dash style names.
-    const linestyles = {
+    static linestyles = {
         solid: [],
         dotted: [1,3],
         dashed: [4,4],
         dashdot: [4,2,1,2],
     }
 
-    // start a new path
-    context.beginPath();
-    // set the line dash pattern
-    context.setLineDash(linestyles[dash]);
+    constructor({label, start, end, color = "black", width = 1, dash ="solid", cap = "round"}) {
+        this.label = label;
+        this.start = new Point(start);
+        this.end = new Point(end);
+        this.color = color;
+        this.width = width;
+        this.dash = dash;
+        this.cap = cap;
+    }
 
-    // Use a Conditional (ternary) operator to swap between regular and inverted y
-    // place the cursor from the point the line should be started
-    context.moveTo(start.x, invert_y ? -start.y : start.y);
-    // draw a line from current cursor position to the provided x,y coordinate
-    context.lineTo(end.x, invert_y ? -end.y : end.y);
+    // Draw a line on the canvas in x,y start to x,y end coordinates
+    draw_line(context) {
 
-    // set stroke Color
-    context.strokeStyle = color;
-    // set stroke Width
-    context.lineWidth = width;
-    // set stroke Cap
-    context.lineCap = cap;
-    // add stroke to the line
-    context.stroke();
+        // start a new path
+        context.beginPath();
+        // set the line dash pattern
+        context.setLineDash(this.linestyles[this.dash]);
+
+        // Use a Conditional (ternary) operator to swap between regular and inverted y
+        // place the cursor from the point the line should be started
+        context.moveTo(this.start.x, this.invert_y ? -this.start.y : this.start.y);
+        // draw a line from current cursor position to the provided x,y coordinate
+        context.lineTo(this.end.x, this.invert_y ? -this.end.y : this.end.y);
+
+        // set stroke Color
+        context.strokeStyle = this.color;
+        // set stroke Width
+        context.lineWidth = this.width;
+        // set stroke Cap
+        context.lineCap = this.cap;
+        // add stroke to the line
+        context.stroke();
+    }
+
+    // Draw text on the canvas at the x and y coordinates
+    draw_text(context) {
+        
+        // Use a conditional (ternary) operator to change the alignment of text based on the active quadrant
+        // The default value is start.
+        context.textAlign = this.end.x > 0 ? "left" : "right";
+        context.textBaseline = this.invert_y ? this.end.y > 0 ? "bottom" : "top" : this.end.y > 0 ? "top" : "bottom";
+        // The default font is 10px sans-serif.
+        context.font = "5px sans-serif"; 
+        context.fillStyle = this.color;
+        context.fillText(this.label, this.end.x, this.invert_y ? -this.end.y : this.end.y);
+    }
+
+    draw(context) {
+        this.draw_line(context);
+        this.draw_text(context);
+    }
 }
 
-// Draw text on the canvas at the x and y coordinates
-function draw_text({context, position, text, color = 'black', invert_y = true, }) {
-    
-    // Use a conditional (ternary) operator to change the alignment of text based on the active quadrant
-    // The default value is start.
-    context.textAlign = position.x > 0 ? "left" : "right";
-    context.textBaseline = invert_y ? position.y > 0 ? "bottom" : "top" : position.y > 0 ? "top" : "bottom";
-    // The default font is 10px sans-serif.
-    context.font = "5px sans-serif"; 
-    context.fillStyle = color;
-    context.fillText(text, position.x, invert_y ? -position.y : position.y);
+function addEventListeners(CANVAS){
+    // For mouse events
+     CANVAS.addEventListener("mousedown", (event) => {console.log(event)});
+    // For touch events
+
 }
 
 function draw_axis({context, translatePos}) {
@@ -86,9 +117,10 @@ function draw_axis({context, translatePos}) {
     }
 }
 
-function plot_graph({scale = 1, translatePos}) {
-    var canvas = document.getElementById("negative_phase_sequence");
-    var context = canvas.getContext("2d");
+function plot_graph({canvas, data, scale = 1, translatePos}) {
+    let context = canvas.getContext("2d");
+
+    addEventListeners(canvas);
 
     // clear canvas
     context.clearRect(0, 0, canvas.width, canvas.height);
@@ -105,24 +137,10 @@ function plot_graph({scale = 1, translatePos}) {
     // Draw the axis lines on the canvas
     draw_axis({context: context, translatePos: translatePos});
 
-    for (const [key, value] of Object.entries(vectors)) {
-
-        // Draw Line
-        draw_line({
-            context: context,
-            start: {x: value.start.x, y: value.start.y},
-            end: {x: value.end.x, y: value.end.y},
-            color: value.color,
-            dash: value.dash,
-        });
-
-        // Draw text
-        draw_text({
-            context: context,
-            position: {x: value.end.x, y: value.end.y},
-            text: value.label,
-            color: value.color,
-        });
+    // Draw the blocks on the canvas
+    for(let i=0; i<VECTORS.length; i++){
+        VECTORS[i].draw(context);
     }
+
     context.restore();
 }
