f540e4cde87a8564398c083fafb3917355a5316d
Neil Bezuidenhout <34944157+TheOtherNeo@users.noreply.github.com>
2023-12-03 20:37:02 +0400

    First Working Commit

--- draw_svg.js
+++ draw_svg.js
@@ -7,140 +7,102 @@ Crated by Neil Bezuidenhout
 20 November 2023
 */
 
-// Global variables will be defined with all caps
-let VECTORS = [];
-
-class Point {
-    constructor(x, y) {
-      this.x = x;
-      this.y = y;
+const removeChilds = (parent) => {
+    while (parent.lastChild) {
+        parent.removeChild(parent.lastChild);
     }
-  
-    static displayName = "Point";
-    static distance(a, b) {
-      const dx = a.x - b.x;
-      const dy = a.y - b.y;
-  
-      return Math.hypot(dx, dy);
-    }
-  }
+};
 
-class Line {
-
-    // Invert all y values.
+class DrawSVG {
+    // Static property to invert the y-axis as 0,0 is top left by default
     static invert_y = true;
+    // Destination DOM to where the elemetns should be appended
+    static context;
 
-    // These are the allowed dash style names.
-    static linestyles = {
-        solid: [],
-        dotted: [1,3],
-        dashed: [4,4],
-        dashdot: [4,2,1,2],
+    // Private method to create a line element: https://developer.mozilla.org/en-US/docs/Web/SVG/Element/line
+    static #line({context, c, x1, y1, x2, y2, stroke="black", text}) {
+        var newLine = document.createElementNS('http://www.w3.org/2000/svg','line');
+        // set the id of the line
+        newLine.setAttribute('id',text);
+        newLine.setAttribute('class',c);
+        newLine.setAttribute('x1',x1);
+        newLine.setAttribute('y1',y1);
+        newLine.setAttribute('x2',x2);
+        newLine.setAttribute('y2',y2);
+        newLine.setAttribute("stroke", stroke)
+        // add the line to the context
+        $(context).append(newLine);
     }
 
-    constructor({label, start, end, color = "black", width = 1, dash ="solid", cap = "round"}) {
-        this.label = label;
-        this.start = new Point(start);
-        this.end = new Point(end);
-        this.color = color;
-        this.width = width;
-        this.dash = dash;
-        this.cap = cap;
+    // Private method to create text element: https://developer.mozilla.org/en-US/docs/Web/SVG/Element/text
+    static #text({context, c, x, y, dx, dy, stroke="black", text}) {
+        var newText = document.createElementNS('http://www.w3.org/2000/svg','text');
+        newText.setAttribute('class',c);
+        newText.setAttribute('x',x);
+        newText.setAttribute('y',y);
+        // newText.setAttribute('dx','-0.5%');
+        // newText.setAttribute('dy','-0.5%');
+        var textNode = document.createTextNode(text);
+        newText.appendChild(textNode);
+        newText.setAttribute("stroke", stroke)
+        $(context).append(newText);
     }
 
     // Draw a line on the canvas in x,y start to x,y end coordinates
-    draw_line(context) {
+    static line({context, c, v, translatePos}) {
 
-        // start a new path
-        context.beginPath();
-        // set the line dash pattern
-        context.setLineDash(this.linestyles[this.dash]);
+        // Need to perform some translations first before passing coordinates to the setAttribute function
+        let x1 = translatePos.x + v.start.x;
+        let y1 = translatePos.y + v.start.y;
+        let x2 = translatePos.x + v.end.x;
+        let y2 = translatePos.y + v.end.y;
+        if (this.invert_y === true) {
+            y1 = translatePos.y - v.start.y;
+            y2 = translatePos.y - v.end.y;
+        }
 
-        // Use a Conditional (ternary) operator to swap between regular and inverted y
-        // place the cursor from the point the line should be started
-        context.moveTo(this.start.x, this.invert_y ? -this.start.y : this.start.y);
-        // draw a line from current cursor position to the provided x,y coordinate
-        context.lineTo(this.end.x, this.invert_y ? -this.end.y : this.end.y);
-
-        // set stroke Color
-        context.strokeStyle = this.color;
-        // set stroke Width
-        context.lineWidth = this.width;
-        // set stroke Cap
-        context.lineCap = this.cap;
-        // add stroke to the line
-        context.stroke();
+        this.#line({context: context, c:c, x1:x1.toFixed(0), y1:y1.toFixed(0), x2:x2.toFixed(0), y2:y2.toFixed(0), text:v.label});
     }
 
-    // Draw text on the canvas at the x and y coordinates
-    draw_text(context) {
-        
-        // Use a conditional (ternary) operator to change the alignment of text based on the active quadrant
-        // The default value is start.
-        context.textAlign = this.end.x > 0 ? "left" : "right";
-        context.textBaseline = this.invert_y ? this.end.y > 0 ? "bottom" : "top" : this.end.y > 0 ? "top" : "bottom";
-        // The default font is 10px sans-serif.
-        context.font = "5px sans-serif"; 
-        context.fillStyle = this.color;
-        context.fillText(this.label, this.end.x, this.invert_y ? -this.end.y : this.end.y);
+
+    // Draw a line on the canvas in x,y start to x,y end coordinates
+    static text({context, c, v, translatePos}) {
+
+        // Need to perform some translations first before passing coordinates to the setAttribute function
+        let x = translatePos.x + v.end.x;
+        let y = translatePos.y + v.end.y;
+
+        if (this.invert_y === true) {
+            y = translatePos.y - v.end.y;
+        }
+
+        this.#text({context: context, c:c, x:x.toFixed(0), y:y.toFixed(0), text:v.label});
     }
 
-    draw(context) {
-        this.draw_line(context);
-        this.draw_text(context);
-    }
-}
-
-function addEventListeners(CANVAS){
-    // For mouse events
-     CANVAS.addEventListener("mousedown", (event) => {console.log(event)});
-    // For touch events
-
-}
-
-function draw_axis({context, translatePos}) {
-    draw_line({context: context, start: {x: -translatePos.x, y: 0}, end: {x: translatePos.x, y: 0}, color: "gray"});
-    draw_line({context: context, start: {x: 0, y: -translatePos.y}, end: {x: 0 , y: translatePos.y}, color: "gray"});
-
-    // Create tick marks on the x-axis
-    for (let i = -translatePos.x; i < translatePos.x; i += 10 ) {
-        if (i === 0) { continue; }  // Skip zero
-        draw_line({context: context, start: {x: i, y: -1}, end: {x: i, y: 0}, color: "gray"});
-        draw_text({context: context, position: {x: i, y: -2}, text: i, color: "gray"});
+    static #axis({context, translatePos}) {
+        // Remember that translatePos will give the centre of the graph, therefore it is o'
+        this.#line({context: context, c:"axis", x1: -translatePos.x, y1: translatePos.y, x2: translatePos.x*2, y2: translatePos.y, stroke: "gray", text:"x-axis"});
+        this.#line({context: context, c:"axis", x1: translatePos.x, y1: -translatePos.y, x2: translatePos.x , y2: translatePos.y*2, stroke: "gray", text:"x-axis"});
+    
     }
 
-    // Create tick marks on the y-axis
-    for (let i = -translatePos.y; i < translatePos.y; i += 10 ) {
-        if (i === 0) { continue; } // Skip zero
-        draw_line({context: context, start: {x: -1, y: i}, end: {x: 0, y: i}, color: "gray"});
-        draw_text({context: context, position: {x: -2, y: i}, text: i, color: "gray"});
+    // Shorthand call to both functions
+    static vector({context, key, value, translatePos}) {
+        this.#axis({context:context, translatePos: translatePos});
+        this.line({context:context, c:key, v:value, translatePos: translatePos});
+        // Don't want to print these values as they are in the same place as another label.
+        if (key!="L1" && key!="L2" && key!="L3") {
+            this.text({context:context, c:key, v:value, translatePos: translatePos});
+        }
     }
 }
 
-function plot_graph({canvas, data, scale = 1, translatePos}) {
-    let context = canvas.getContext("2d");
+function plot_svg({context, scale, translatePos}) {
 
-    addEventListeners(canvas);
+    // remove all child nodes
+    removeChilds(context);
 
-    // clear canvas
-    context.clearRect(0, 0, canvas.width, canvas.height);
-
-    // Save the current state of the canvas translations.
-    context.save();
-
-    // Shift the centre point of the canvas
-    context.translate(translatePos.x, translatePos.y);
-
-    // Adjust the scaling based on the zoom slider
-    context.scale(scale, scale);
-
-    // Draw the axis lines on the canvas
-    draw_axis({context: context, translatePos: translatePos});
-
-    // Draw the blocks on the canvas
-    for(let i=0; i<VECTORS.length; i++){
-        VECTORS[i].draw(context);
+    for (let [key, value] of Object.entries(vectors)) {
+        DrawSVG.vector({context:context, key:key, value:value, translatePos: translatePos});
     }
-
-    context.restore();
 }
